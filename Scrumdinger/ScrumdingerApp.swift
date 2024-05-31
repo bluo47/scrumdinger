a2ae91a38391a04105cb44ad42b787e5c979bade
Benjamin Luo <bpla2019@mymail.pomona.edu>
2024-05-30 20:12:14 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Benjamin Luo on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
